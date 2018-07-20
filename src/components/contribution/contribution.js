15b6351c7663313c9fe973539847ae2c660a730a
Kenny <khpham24@gmail.com>
2018-07-20 10:48:47 -0600

    added stuffs

--- src/components/contribution/contribution.js
+++ src/components/contribution/contribution.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import './contribution.css';
 
-// I a
 class Contribution extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
-    return <div className=""> </div>;
+    return (
+      <div className="Contribution">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="Date"
+            type="date"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input
+            className="Activity"
+            type="text"
+            placeholder="Contribution"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input
+            className="Link"
+            type="text"
+            placeholder="Link"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input
+            className="Notes"
+            type="text"
+            placeholder="Notes"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
--- src/components/meetup/meetup.js
+++ src/components/meetup/meetup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import './meetup.css';
 
-class Meetup extends React.Component {
+class Meetup extends Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
@@ -20,17 +21,38 @@ class Meetup extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
+      <div className="Meetup">
+        <form onSubmit={this.handleSubmit}>
           <input
-            type="text"
+            className="Date"
+            type="date"
             value={this.state.value}
             onChange={this.handleChange}
           />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          <input
+            className="Activity"
+            type="text"
+            placeholder="Activity"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input
+            className="Link"
+            type="text"
+            placeholder="Link"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input
+            className="Notes"
+            type="text"
+            placeholder="Notes"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
